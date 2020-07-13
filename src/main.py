b632e0927f32b22c0dbaa1beef0a0cc4ca40229f
Robin_tty <muellerobin95@gmail.com>
2020-07-13 16:36:33 +0200

    update exception handling

--- src/main.py
+++ src/main.py
@@ -10,24 +10,25 @@ def main():
     countries = ["Germany"]  # maximum is 5
 
     start_time = time.time()
-    try:
-        while True:
+    while True:
+        try:
             new_cases = DataProvider.get_new_cases_day(countries)
+        except Exception as exception:
+            exception = "Unexpected error of type" + str(type(exception)) + ":" + str(exception)
+            print(exception)
+            with open("log.txt", "a+") as f:
+                f.write(str(exception))
+            new_cases = []
 
-            # only print if numbers changed
-            cases = [cases[1] for cases in new_cases]
-            if not array_equal(cases, last_cycle):
-                inky.create_new_image(new_cases)
+        # only print if numbers changed
+        cases = [cases[1] for cases in new_cases]
+        if not array_equal(cases, last_cycle):
+            inky.create_new_image(new_cases)
 
-            last_cycle = cases
-            # sleep 2 minutes
-            time.sleep(120.0 - ((time.time() - start_time) % 120.0))
-    except Exception as exception:
-        exception = "Unexpected error of type" + str(type(exception)) + ":" + str(exception)
-        print(exception)
-        with open("log.txt", "a+") as f:
-            f.write(str(exception))
-
+        last_cycle = cases
+        # sleep 2 minutes
+        time.sleep(120.0 - ((time.time() - start_time) % 120.0))
+    
 
 if __name__ == '__main__':
     main()
